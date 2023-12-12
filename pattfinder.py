9eeef381e73b88ea3585a6d2be9cffad7701fcc1
Ale Santoro <alexi.santoro@gmail.com>
2023-12-13 00:00:00 +0100

    added comments

--- pattfinder.py
+++ pattfinder.py
@@ -1,39 +1,45 @@
 import sys
-import tkinter as tk
-from itertools import permutations
-from itertools import combinations
+from itertools import permutations,combinations
 
-class PatternAvoid(object):
+class PatternAvoid():
 	def __init__(self,n, pattern):
+		self.__n = n
 		if n <= 1:
 			print("ERROR: expecting a whole number grater than 1 but got: " + str(n))
 			exit()
 
+		# checking the argument really is a classical pattern
 		for i in range(1, len(pattern)):
 			if i not in pattern:
 				print("ERROR: expected a pattern but got: " + str(pattern))
 				print("\nA pattern of lenght n must have all the numbers from 1 to n, " + str(i) + " not present")
 				exit()
 
-		perms = list(permutations(range(1,n+1)))
+		# generating permutations
+		permutations_list = list(permutations(range(1,n+1)))
 
-		self.notcontaining = []
-		self.containing = []
+		# declaring list fields
+		self.__notcontaining = []
+		self.__containing = []
 
-		for p in perms:
+		for p in permutations_list:
 			if not self.contains(p,pattern):
-				self.notcontaining.append(p)
+				self.__notcontaining.append(p)
 			else:
-				self.containing.append(p)
+				self.__containing.append(p)
 
-		def patternize(self,pi):
-			output = []
-			sortedpi = sorted(pi)
-			for p in pi:
-				output.append(sortedpi.index(p)+1)
-			return output
+	# get the standard image of the number sequence:
+	def patternize(self,pi):
+		output = []
+		id_ = sorted(pi)
+		for p in pi:
+			output.append(id_.index(p)+1)
+		return output
 
+	# check if a seqence contains the given pattern
 	def contains(self, seq, pattern):
+		if len(seq) < len(pattern):
+			return False
 		subseq = combinations(seq, len(pattern))
 		for s in subseq:
 			if list(self.patternize(s)) == list(pattern):
@@ -41,10 +47,10 @@ class PatternAvoid(object):
 		return False
 
 	def getNotContaining(self):
-		return self.notcontaining
+		return self.__notcontaining
 
 	def getContaining(self):
-		return self.containing
+		return self.__containing
 
 def printlist(list):
 	result = ""
@@ -58,6 +64,7 @@ if __name__ == '__main__':
 	
 	if narg ==1:
 		# for future implementation of GUI interface
+		import tkinter as tk
 		print("GUI not implemented yet, use command line")
 
 	elif(narg==3):
@@ -71,9 +78,9 @@ if __name__ == '__main__':
 		ncon = pa.getNotContaining()
 
 		ncon_str = "\nThe following "+str(len(ncon))+" " + str(n)+"-permutations do not contain the pattern " + str_pattern+":\n\n"+printlist(ncon)
-		con_str = "\nThe following "+str(len(con))+" " + str(n)+"-permutations do contain the pattern "  + str_pattern+":\n\n"+printlist(con)
+		con_str = "The following "+str(len(con))+" " + str(n)+"-permutations do contain the pattern "  + str_pattern+":\n\n"+printlist(con)
 
-		print(ncon_str+"\n")
+		print(ncon_str)
 		print(con_str)
 
 		# only the not conatining get saved in a file
